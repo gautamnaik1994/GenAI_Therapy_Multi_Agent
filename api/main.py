e61dc9a1e4ce1850f256461a3005d7b5c123fb56
Gautam Naik <gautamnaik1994@gmail.com>
2025-05-31 23:54:09 +0530

    fix cors issue

--- api/main.py
+++ api/main.py
@@ -16,7 +16,7 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://psytrackr.netlify.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
